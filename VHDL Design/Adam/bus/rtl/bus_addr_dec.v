f09528f1f96d8baf59b31be7b6004741778c99ab
Rick_Ming <42510322+NUISTGY@users.noreply.github.com>
2020-09-05 20:43:57 +0800

    总线架构

---
+++ VHDL Design/Adam/bus/rtl/bus_addr_dec.v
@@ -0,0 +1,65 @@
+`include "nettype.h"
+`include "stddef.h"
+`include "global_config.h"
+
+`include "bus.h"
+
+/********** 地址译码器 **********/
+module bus_addr_dec (
+
+	input  wire [`WordAddrBus] s_addr, 
+
+	output reg				   s0_cs_, 
+	output reg				   s1_cs_, 
+	output reg				   s2_cs_, 
+	output reg				   s3_cs_, 
+	output reg				   s4_cs_, 
+	output reg				   s5_cs_, 
+	output reg				   s6_cs_, 
+	output reg				   s7_cs_  
+);
+
+	
+	wire [`BusSlaveIndexBus] s_index = s_addr[`BusSlaveIndexLoc];
+
+
+	always @(*) begin
+	
+		s0_cs_ = `DISABLE_;
+		s1_cs_ = `DISABLE_;
+		s2_cs_ = `DISABLE_;
+		s3_cs_ = `DISABLE_;
+		s4_cs_ = `DISABLE_;
+		s5_cs_ = `DISABLE_;
+		s6_cs_ = `DISABLE_;
+		s7_cs_ = `DISABLE_;
+	
+		case (s_index)
+			`BUS_SLAVE_0 : begin 
+				s0_cs_	= `ENABLE_;
+			end
+			`BUS_SLAVE_1 : begin 
+				s1_cs_	= `ENABLE_;
+			end
+			`BUS_SLAVE_2 : begin 
+				s2_cs_	= `ENABLE_;
+			end
+			`BUS_SLAVE_3 : begin 
+				s3_cs_	= `ENABLE_;
+			end
+			`BUS_SLAVE_4 : begin 
+				s4_cs_	= `ENABLE_;
+			end
+			`BUS_SLAVE_5 : begin 
+				s5_cs_	= `ENABLE_;
+			end
+			`BUS_SLAVE_6 : begin 
+				s6_cs_	= `ENABLE_;
+			end
+			`BUS_SLAVE_7 : begin 
+				s7_cs_	= `ENABLE_;
+			end
+		endcase
+	end
+
+endmodule
---
+++ VHDL Design/Adam/bus/rtl/bus_arbiter.v
@@ -0,0 +1,110 @@
+`include "nettype.h"
+`include "stddef.h"
+`include "global_config.h"
+
+`include "bus.h"
+
+/********** 主控仲裁器 **********/
+module bus_arbiter (
+
+	input  wire		   clk,		 
+	input  wire		   reset,	 
+	
+	input  wire		   m0_req_,	 
+	output reg		   m0_grnt_, 
+	
+	input  wire		   m1_req_,	 
+	output reg		   m1_grnt_, 
+	
+	input  wire		   m2_req_,	 
+	output reg		   m2_grnt_, 
+	
+	input  wire		   m3_req_,	 
+	output reg		   m3_grnt_	 
+);
+
+	reg [`BusOwnerBus] owner;	 
+   
+	always @(*) begin
+
+		m0_grnt_ = `DISABLE_;
+		m1_grnt_ = `DISABLE_;
+		m2_grnt_ = `DISABLE_;
+		m3_grnt_ = `DISABLE_;
+
+		case (owner)
+			`BUS_OWNER_MASTER_0 : begin 
+				m0_grnt_ = `ENABLE_;
+			end
+			`BUS_OWNER_MASTER_1 : begin 
+				m1_grnt_ = `ENABLE_;
+			end
+			`BUS_OWNER_MASTER_2 : begin 
+				m2_grnt_ = `ENABLE_;
+			end
+			`BUS_OWNER_MASTER_3 : begin 
+				m3_grnt_ = `ENABLE_;
+			end
+		endcase
+	end
+   
+	always @(posedge clk or `RESET_EDGE reset) begin
+		if (reset == `RESET_ENABLE) begin
+
+			owner <= #1 `BUS_OWNER_MASTER_0;
+		end else begin
+
+			case (owner)
+				`BUS_OWNER_MASTER_0 : begin 
+	
+					if (m0_req_ == `ENABLE_) begin			
+						owner <= #1 `BUS_OWNER_MASTER_0;
+					end else if (m1_req_ == `ENABLE_) begin 
+						owner <= #1 `BUS_OWNER_MASTER_1;
+					end else if (m2_req_ == `ENABLE_) begin 
+						owner <= #1 `BUS_OWNER_MASTER_2;
+					end else if (m3_req_ == `ENABLE_) begin 
+						owner <= #1 `BUS_OWNER_MASTER_3;
+					end
+				end
+				`BUS_OWNER_MASTER_1 : begin 
+		
+					if (m1_req_ == `ENABLE_) begin			
+						owner <= #1 `BUS_OWNER_MASTER_1;
+					end else if (m2_req_ == `ENABLE_) begin 
+						owner <= #1 `BUS_OWNER_MASTER_2;
+					end else if (m3_req_ == `ENABLE_) begin 
+						owner <= #1 `BUS_OWNER_MASTER_3;
+					end else if (m0_req_ == `ENABLE_) begin 
+						owner <= #1 `BUS_OWNER_MASTER_0;
+					end
+				end
+				`BUS_OWNER_MASTER_2 : begin 
+		
+					if (m2_req_ == `ENABLE_) begin			
+						owner <= #1 `BUS_OWNER_MASTER_2;
+					end else if (m3_req_ == `ENABLE_) begin 
+						owner <= #1 `BUS_OWNER_MASTER_3;
+					end else if (m0_req_ == `ENABLE_) begin 
+						owner <= #1 `BUS_OWNER_MASTER_0;
+					end else if (m1_req_ == `ENABLE_) begin 
+						owner <= #1 `BUS_OWNER_MASTER_1;
+					end
+				end
+				`BUS_OWNER_MASTER_3 : begin 
+	
+					if (m3_req_ == `ENABLE_) begin			
+						owner <= #1 `BUS_OWNER_MASTER_3;
+					end else if (m0_req_ == `ENABLE_) begin 
+						owner <= #1 `BUS_OWNER_MASTER_0;
+					end else if (m1_req_ == `ENABLE_) begin 
+						owner <= #1 `BUS_OWNER_MASTER_1;
+					end else if (m2_req_ == `ENABLE_) begin 
+						owner <= #1 `BUS_OWNER_MASTER_2;
+					end
+				end
+			endcase
+		end
+	end
+
+endmodule
